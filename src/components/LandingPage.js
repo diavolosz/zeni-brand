dac73fc86366b99f945c9050b02d865a820f4bdb
michael <michael.leduong@vretta.com>
2023-06-22 09:57:02 -0400

    added animation to intro page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,52 @@
 
 
+import { useEffect } from "react"
+import { gsap } from "gsap"
 import "./LandingPage.scss"
 
 
 export default function LandingPage() {
+
+  useEffect(() => {
+
+    let ctx = gsap.context(() => {
+      gsap.from(".cinematic-shot", {
+        delay: 3,
+        duration: 1,
+        scale: 1.5,
+        ease: "power2"
+      })
+
+      gsap.from(".left", {
+        delay: 3,
+        duration: 1.5,
+        x: "-300",
+        opacity: 0,
+        ease: "power2"
+      })
+
+      gsap.from(".right", {
+        delay: 3,
+        duration: 1.25,
+        x: "300",
+        opacity: 0,
+        ease: "power1"
+      })
+
+    })
+    return () => ctx.revert();
+  }, [])
+
   return (
     <div className="page-container">
       <div className="image-wrapper">
         <img className="cinematic-shot" src="img/cinematic-shot.png" alt="landing-page-shot"></img>
+        <div className="about-us">
+          <span className="left">About</span> <span className="right">Us</span>
+        </div>
       </div>
+
+
 
       <section className="block-wrapper block-1">
         this is block 1
