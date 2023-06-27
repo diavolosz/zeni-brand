613be8dad7ce0fff64d89bbc8e15c775c288811f
michael-PC <michaeldiavolos@gmail.com>
2023-06-26 21:39:04 -0400

    completed block 1 of landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -49,14 +49,34 @@ export default function LandingPage() {
 
 
       <section className="block-wrapper block-1">
-        this is block 1
+
+        <div className="description-1 small-des">
+          <span>ZENI aims to bring a sense of calm and tranquility to the wearer, allowing them to embrace their individuality and express with grace and poise</span>
+        </div>
+
+        <div className="description-2">
+          <span>
+          Serenity of Style - we strive to create a <i>peaceful</i> and <i>elegant</i> experience, allowing our customers to connect with their <i>inner serenity</i> while making a positive impact on the world around them
+          </span>
+        </div>
+
+        <div className="description-3 small-des">
+          <span>scroll down to learn more about all of our mission & team</span>
+        </div>
+
       </section>
-      <section className="block-wrapper block-2">
+
+
+
+      {/* <section className="block-wrapper block-2">
         this is block 2
       </section>
+
+
+
       <section className="block-wrapper block-3">
         this is block 3
-      </section>
+      </section> */}
     </div>
   )
 }
