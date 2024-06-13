4e30a6546e46d66e1fc9243691dbcc160ed7ecfe
diavolos <michaeldiavolos@gmail.com>
2024-06-13 16:02:19 -0400

    timeoout

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -21,32 +21,32 @@ export default function LandingPage(props) {
   gsap.registerPlugin(InertiaPlugin)
 
   let imageDataSet = [
-    {src: "img/draggables/D11.jpg", sizeClass: "class-1", offSetFactor: -0.05,   yOffSet: 5,  xOffSet: 2,     displayDelay: 4.16},
-    {src: "img/draggables/D5.jpg",  sizeClass: "class-2", offSetFactor: -0.08,   yOffSet: 3,  xOffSet: -5,    displayDelay: 4.10},
-    {src: "img/draggables/D20.jpg",  sizeClass: "class-1", offSetFactor: -0.05,  yOffSet: 5,  xOffSet: -4,    displayDelay: 4.02},
-    {src: "img/draggables/D2.jpg",  sizeClass: "class-3", offSetFactor: -0.1,    yOffSet: 4,  xOffSet: -5,    displayDelay: 4.04},
-    {src: "img/draggables/D3.jpg",  sizeClass: "class-4", offSetFactor: -0.005,  yOffSet: 2,  xOffSet: 0,     displayDelay: 3.97},
-    {src: "img/draggables/D6.jpg",  sizeClass: "class-5", offSetFactor: -0.01,   yOffSet: 3,  xOffSet: -2,    displayDelay: 4.41},
-    {src: "img/draggables/D7.jpg",  sizeClass: "class-6", offSetFactor: -0.08,   yOffSet: 0,  xOffSet: -6,    displayDelay: 4.29},
-    {src: "img/draggables/D8.jpg",  sizeClass: "class-7", offSetFactor: -0.06,   yOffSet: 0,  xOffSet: -9,    displayDelay: 3.71},
-    {src: "img/draggables/D9.jpg",  sizeClass: "class-8", offSetFactor: -0.08,   yOffSet: -3, xOffSet: -6,    displayDelay: 4.13},
-    {src: "img/draggables/D10.jpg", sizeClass: "class-1", offSetFactor: -0.1,    yOffSet: 0,  xOffSet: -3,    displayDelay: 3.51},
-    {src: "img/draggables/D1.jpg",  sizeClass: "class-6", offSetFactor: -0.05,   yOffSet: 0,  xOffSet: -4,    displayDelay: 4.29},
-    {src: "img/draggables/D12.jpg", sizeClass: "class-7", offSetFactor: -0.03,   yOffSet: 1,  xOffSet: -5,    displayDelay: 3.86},
-    {src: "img/draggables/D13.jpg", sizeClass: "class-10", offSetFactor: -0.06,  yOffSet: 0,  xOffSet: -3,    displayDelay: 4.34},
-    {src: "img/draggables/D18.jpg", sizeClass: "class-2", offSetFactor: -0.08,   yOffSet: 0,  xOffSet: -4.5,  displayDelay: 3.77},
-    {src: "img/draggables/D15.jpg", sizeClass: "class-3", offSetFactor: -0.02,   yOffSet: 4,  xOffSet: 1,     displayDelay: 3.57},
-    {src: "img/draggables/D16.jpg", sizeClass: "class-6", offSetFactor: -0.04,   yOffSet: 4,  xOffSet: -2,    displayDelay: 4.01},
-    {src: "img/draggables/D17.jpg", sizeClass: "class-1", offSetFactor: -0.09,   yOffSet: 0,  xOffSet: -1,    displayDelay: 3.57},
-    {src: "img/draggables/D14.jpg", sizeClass: "class-6", offSetFactor: -0.05,   yOffSet: 3,  xOffSet: 2,     displayDelay: 4.43},
-    {src: "img/draggables/D19.jpg", sizeClass: "class-3", offSetFactor: -0.08,   yOffSet: 0,  xOffSet: 0,     displayDelay: 3.88},
-    {src: "img/draggables/D4.jpg", sizeClass: "class-6", offSetFactor: -0.04,    yOffSet: 0,  xOffSet: -4,    displayDelay: 4.45},
-    {src: "img/draggables/D21.jpg", sizeClass: "class-7", offSetFactor: -0.1,    yOffSet: 0,  xOffSet: -3,    displayDelay: 3.97},
-    {src: "img/draggables/D22.jpg", sizeClass: "class-6", offSetFactor: -0.06,   yOffSet: 0,  xOffSet: 0,     displayDelay: 3.67},
-    {src: "img/draggables/D23.jpg", sizeClass: "class-10", offSetFactor: -0.03,  yOffSet: 0,  xOffSet: -3,    displayDelay: 4.43},
-    {src: "img/draggables/D24.jpg", sizeClass: "class-5", offSetFactor: -0.07,   yOffSet: 0,  xOffSet: -4,    displayDelay: 4.18},
-    {src: "img/draggables/D26.jpg", sizeClass: "class-10", offSetFactor: -0.03,  yOffSet: -1, xOffSet: 0,     displayDelay: 4.43},
-    {src: "img/draggables/D25.jpg", sizeClass: "class-9", offSetFactor: 0,       yOffSet: 0,  xOffSet: -4,    displayDelay: 4.34},
+    { src: "img/draggables/D11.jpg", sizeClass: "class-1", offSetFactor: -0.05, yOffSet: 5, xOffSet: 2, displayDelay: 4.16 },
+    { src: "img/draggables/D5.jpg", sizeClass: "class-2", offSetFactor: -0.08, yOffSet: 3, xOffSet: -5, displayDelay: 4.10 },
+    { src: "img/draggables/D20.jpg", sizeClass: "class-1", offSetFactor: -0.05, yOffSet: 5, xOffSet: -4, displayDelay: 4.02 },
+    { src: "img/draggables/D2.jpg", sizeClass: "class-3", offSetFactor: -0.1, yOffSet: 4, xOffSet: -5, displayDelay: 4.04 },
+    { src: "img/draggables/D3.jpg", sizeClass: "class-4", offSetFactor: -0.005, yOffSet: 2, xOffSet: 0, displayDelay: 3.97 },
+    { src: "img/draggables/D6.jpg", sizeClass: "class-5", offSetFactor: -0.01, yOffSet: 3, xOffSet: -2, displayDelay: 4.41 },
+    { src: "img/draggables/D7.jpg", sizeClass: "class-6", offSetFactor: -0.08, yOffSet: 0, xOffSet: -6, displayDelay: 4.29 },
+    { src: "img/draggables/D8.jpg", sizeClass: "class-7", offSetFactor: -0.06, yOffSet: 0, xOffSet: -9, displayDelay: 3.71 },
+    { src: "img/draggables/D9.jpg", sizeClass: "class-8", offSetFactor: -0.08, yOffSet: -3, xOffSet: -6, displayDelay: 4.13 },
+    { src: "img/draggables/D10.jpg", sizeClass: "class-1", offSetFactor: -0.1, yOffSet: 0, xOffSet: -3, displayDelay: 3.51 },
+    { src: "img/draggables/D1.jpg", sizeClass: "class-6", offSetFactor: -0.05, yOffSet: 0, xOffSet: -4, displayDelay: 4.29 },
+    { src: "img/draggables/D12.jpg", sizeClass: "class-7", offSetFactor: -0.03, yOffSet: 1, xOffSet: -5, displayDelay: 3.86 },
+    { src: "img/draggables/D13.jpg", sizeClass: "class-10", offSetFactor: -0.06, yOffSet: 0, xOffSet: -3, displayDelay: 4.34 },
+    { src: "img/draggables/D18.jpg", sizeClass: "class-2", offSetFactor: -0.08, yOffSet: 0, xOffSet: -4.5, displayDelay: 3.77 },
+    { src: "img/draggables/D15.jpg", sizeClass: "class-3", offSetFactor: -0.02, yOffSet: 4, xOffSet: 1, displayDelay: 3.57 },
+    { src: "img/draggables/D16.jpg", sizeClass: "class-6", offSetFactor: -0.04, yOffSet: 4, xOffSet: -2, displayDelay: 4.01 },
+    { src: "img/draggables/D17.jpg", sizeClass: "class-1", offSetFactor: -0.09, yOffSet: 0, xOffSet: -1, displayDelay: 3.57 },
+    { src: "img/draggables/D14.jpg", sizeClass: "class-6", offSetFactor: -0.05, yOffSet: 3, xOffSet: 2, displayDelay: 4.43 },
+    { src: "img/draggables/D19.jpg", sizeClass: "class-3", offSetFactor: -0.08, yOffSet: 0, xOffSet: 0, displayDelay: 3.88 },
+    { src: "img/draggables/D4.jpg", sizeClass: "class-6", offSetFactor: -0.04, yOffSet: 0, xOffSet: -4, displayDelay: 4.45 },
+    { src: "img/draggables/D21.jpg", sizeClass: "class-7", offSetFactor: -0.1, yOffSet: 0, xOffSet: -3, displayDelay: 3.97 },
+    { src: "img/draggables/D22.jpg", sizeClass: "class-6", offSetFactor: -0.06, yOffSet: 0, xOffSet: 0, displayDelay: 3.67 },
+    { src: "img/draggables/D23.jpg", sizeClass: "class-10", offSetFactor: -0.03, yOffSet: 0, xOffSet: -3, displayDelay: 4.43 },
+    { src: "img/draggables/D24.jpg", sizeClass: "class-5", offSetFactor: -0.07, yOffSet: 0, xOffSet: -4, displayDelay: 4.18 },
+    { src: "img/draggables/D26.jpg", sizeClass: "class-10", offSetFactor: -0.03, yOffSet: -1, xOffSet: 0, displayDelay: 4.43 },
+    { src: "img/draggables/D25.jpg", sizeClass: "class-9", offSetFactor: 0, yOffSet: 0, xOffSet: -4, displayDelay: 4.34 },
   ]
 
 
@@ -55,64 +55,66 @@ export default function LandingPage(props) {
 
   const dragRef = useRef(null);
 
-  const {view} = props
+  const { view } = props
 
   useEffect(() => {
-    let InitialPosition
-    
-    const imgDraggable = Draggable.create(".image-set", {
-      type: 'x',
-      bounds: ".scroller-container",
-      edgeResistance: 0.8,
-      dragResistance: 0.3,
-      throwProps: true,
-      overshootTolerance: 0.5,
-      inertia: true,
-      cursor: 'grab',
-      zIndexBoost: false,
-      onDrag: function() {
-        setXPosition((this.x - InitialPosition)/InitialPosition*100)
-        // console.log('dragging', (this.x - InitialPosition))
-        // console.log('dragging', (this.x - InitialPosition)/InitialPosition*100)
-        gsap.to('.text',{opacity: 0, duration: 0.8})
-      },
-      onThrowUpdate: function () {
-        setXPosition((this.x - InitialPosition)/InitialPosition*100)
-        // console.log('throwing', (this.x - InitialPosition))
-        // console.log('throwing', (this.x - InitialPosition)/InitialPosition*100)
-      },
-      onThrowComplete: function() {
-        gsap.to('.text',{opacity: 1, duration: 0.8})
-      }
-    })[0];
-
-    //find the width of draggable to set mid point on initial
-    let width = imgDraggable.minX
-    gsap.set('.image-set', {x: `${width/2}`})
-
-    //set initial position for future update 
-    InitialPosition = imgDraggable.x
-
-    // Event listener for window resize
-    // Add event listener for window resize
-    const handleResize = () => { setWindowWidth(window.innerWidth) };
-    window.addEventListener('resize', handleResize);
+    setTimeout(() => {
 
 
+      let InitialPosition
+
+      const imgDraggable = Draggable.create(".image-set", {
+        type: 'x',
+        bounds: ".scroller-container",
+        edgeResistance: 0.8,
+        dragResistance: 0.3,
+        throwProps: true,
+        overshootTolerance: 0.5,
+        inertia: true,
+        cursor: 'grab',
+        zIndexBoost: false,
+        onDrag: function () {
+          setXPosition((this.x - InitialPosition) / InitialPosition * 100)
+          // console.log('dragging', (this.x - InitialPosition))
+          // console.log('dragging', (this.x - InitialPosition)/InitialPosition*100)
+          gsap.to('.text', { opacity: 0, duration: 0.8 })
+        },
+        onThrowUpdate: function () {
+          setXPosition((this.x - InitialPosition) / InitialPosition * 100)
+          // console.log('throwing', (this.x - InitialPosition))
+          // console.log('throwing', (this.x - InitialPosition)/InitialPosition*100)
+        },
+        onThrowComplete: function () {
+          gsap.to('.text', { opacity: 1, duration: 0.8 })
+        }
+      })[0];
+
+      //find the width of draggable to set mid point on initial
+      let width = imgDraggable.minX
+      gsap.set('.image-set', { x: `${width / 2}` })
+
+      //set initial position for future update 
+      InitialPosition = imgDraggable.x
+
+      // Event listener for window resize
+      // Add event listener for window resize
+      const handleResize = () => { setWindowWidth(window.innerWidth) };
+      window.addEventListener('resize', handleResize);
+
+    }, 100);
   }, [windowWidth]);
 
 
 
   useEffect(() => {
-    setWindowWidth(window.innerWidth)
     let imgDisplayArr = document.querySelectorAll('.imgDisplay')
     imgDisplayArr.forEach((el, index) => {
       gsap.fromTo(el, {
         opacity: 0
-      },{
+      }, {
         opacity: 1,
         duration: 1.8 + Math.random() * (2.5 - 1.8),
-        delay: imageDataSet[index].displayDelay-0.08
+        delay: imageDataSet[index].displayDelay - 0.08
       })
     })
   }, [])
@@ -125,15 +127,15 @@ export default function LandingPage(props) {
 
         <div ref={dragRef} className='image-set'>
           {imageDataSet.map((imgData, index) => (
-            <img 
-              key={index} 
-              src={imgData.src} 
-              className={`${imgData.sizeClass} imgDisplay`} 
+            <img
+              key={index}
+              src={imgData.src}
+              className={`${imgData.sizeClass} imgDisplay`}
               // style={{transform: `translateX(${imgData?.xOffSet}px)`}}
               style={{
-                transform: `translateX(${(xPosition * imgData?.offSetFactor)+ imgData?.xOffSet}dvw) translateY(${imgData.yOffSet}dvh)`,
-              }}              
-              alt={imgData.src} 
+                transform: `translateX(${(xPosition * imgData?.offSetFactor) + imgData?.xOffSet}dvw) translateY(${imgData.yOffSet}dvh)`,
+              }}
+              alt={imgData.src}
             />
           ))}
         </div>
